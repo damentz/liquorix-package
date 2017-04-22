a996f8230d8b1da9b21b1e1ac227412353e0e925
Steven Barrett <steven@liquorix.net>
2017-04-22 16:37:23 -0500

    Add pull-stable-queue.sh
    This script can be used to merge all upcoming release candidate patches
    for the next version of a kernel.  It requires that you are already in
    the directory of your kernel git repository and that you provide a
    single parameter, the directory of the stable-queue repository from:
    
    https://git.kernel.org/pub/scm/linux/kernel/git/stable/stable-queue.git/

---
+++ scripts/pull-stable-queue.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$1" ]] || [[ ! -d "$1" ]]; then
+	echo "[ERROR] '$1' is an invalid directory."
+    echo "Usage: $0 [directory]"
+	exit 1
+fi
+
+branch="$(git branch | grep -E '^\* [0-9]+\.[0-9]+/upstream-updates-next')"
+version="$(echo "$branch" | grep -Eo '[0-9]+\.[0-9]+')"
+queue="$1/queue-$version"
+
+if [[ "$branch" =~ upstream-updates-next ]]; then
+	echo "[INFO ] branch is valid"
+else
+	echo "[ERROR] branch, $branch, is invalid"
+	exit 1
+fi
+
+if [[ "$version" =~ [0-9]+\.[0-9]+ ]]; then
+	echo "[INFO ] version is valid"
+else 
+	"[ERROR] version, $version, is invalid"
+	exit 1
+fi
+
+echo "[INFO ] fetching latest changes"
+git fetch linux-stable
+
+echo "[INFO ] resetting repository"
+git reset --hard linux-stable/linux-$version.y
+
+echo "[INFO ] cleaning repository"
+git clean -xdf
+
+echo "[INFO ] checking if, $queue, exists"
+stat "$queue" &> /dev/null
+if [[ "$?" -ne 0 ]]; then
+	echo "[ERROR] folder, $queue, does not exist"	
+	exit 1
+fi
+
+echo "[INFO ] merging from stable queue"
+for file in $(cat $queue/series); do
+	git am -3 "$queue/$file"
+done
+
+exit 0
