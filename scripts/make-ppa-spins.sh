b407da532fced6b1d1a3d30111ae75c62f429e87
Steven Barrett <steven@liquorix.net>
2017-11-13 22:15:07 -0600

    Implement naive source package build and dput execution

--- scripts/make-ppa-spins.sh
+++ scripts/make-ppa-spins.sh
@@ -36,19 +36,57 @@ function prepare_env {
 	fi
 
 	cd "$dir_package"
-	echo "[INFO ] Cleaning package"
+	echo "[INFO ] Cleaning $package_name"
 	fakeroot debian/rules maintainerclean
 
-	echo "[INFO ] Copying over $package_name package"
+	echo "[INFO ] Copying $package_name to $dir_ppa/"
 	cp -raf "$dir_package/" "$dir_ppa/"
 
 	if [[ ! -f "$dir_base/$package_source" ]]; then
 		echo "[WARN ] Missing source file: $dir_base/$package_source, downloading now."
-		wget -O "$dir_base/$package_source" "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$version_package.tar.xz"
+		wget -O "$dir_base/$package_source" "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-${version_kernel}.tar.xz"
+	fi
+
+	if [[ ! -L "$dir_ppa/$package_source" ]]; then
+		echo "[INFO ] Missing symlink: $dir_ppa/$package_source, creating"
+		ln -sf "$dir_base/$package_source" "$dir_ppa/$package_source"
 	fi
 
 	echo "[INFO ] Unpacking kernel source into package folder."
 	tar -xf "$dir_base/$package_source" --strip-components=1 -C "$dir_ppa/$package_name"
 }
 
+function build_source_package {
+	local release="$1"
+	local version_ppa="$2"
+
+	cd "$dir_ppa/$package_name"
+
+	echo "[INFO ] Updating changelog to: $version_ppa"
+	sed -r -i "1s/[^;]+(;.*)/$package_name ($version_ppa) $release\1/" debian/changelog
+
+	echo "[INFO ] Stripping gcc version (use release native gcc)"
+	sed -r -i "s/gcc-[0-9]+/gcc/g" debian/config/defines
+
+	echo "[INFO ] Cleaning package"
+	local clean='fakeroot debian/rules clean'
+	$clean || $clean
+
+	echo "[INFO ] Making source package"
+	debuild --no-lintian -S -sa
+
+	echo "[INFO ] Submitting"
+}
+
 prepare_env
+
+for release in "${releases[@]}"; do
+	version_ppa="${version_package}ubuntu1~${release}"
+
+	echo "[INFO ] Building source package for $release"
+	build_source_package "$release" "$version_ppa"
+
+	echo "[INFO ] Uploading packages to Launchpad PPA"
+	dput 'liquorix' "${dir_ppa}/${package_name}_${version_ppa}_source.changes" ||
+		{ echo "[ERROR] dput failed to push package!"; }
+done
