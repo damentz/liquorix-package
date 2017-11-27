2ec9d6fb1c3b202d8eae9f27ca0404dd9b21ae0c
Steven Barrett <steven@liquorix.net>
2017-11-26 18:46:50 -0600

    Add script to delete superseded PPA packages

---
+++ scripts/delete-ppa-packages.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+from launchpadlib.launchpad import Launchpad
+from launchpadlib.credentials import Credentials
+
+if __name__ == "__main__":
+    credentials = Credentials("Liquorix")
+    request_token_info = credentials.get_request_token(web_root="production")
+
+    cachedir = os.path.expanduser('~/.launchpadlib/cache')
+    launchpad = Launchpad.login_with(
+        'Liquorix', 'production', cachedir, version='devel')
+    ppa = launchpad.me.getPPAByName(name="liquorix")
+
+    # We can either user superseded sources or binaries.  Sources supersede
+    # immediately while binaries only superseded after a new binary has been
+    # published.  This works in our favor since we only want to delete the
+    # package if there's a new version available for download.
+    superseded_binaries = ppa.getPublishedBinaries(status="Superseded")
+
+    if superseded_binaries:
+        for entry in superseded_binaries.entries:
+
+            # Deleted packages have a time stamp for their date_removed
+            # attribute.  No need to request deletion on these.
+            if entry['date_removed']:
+                continue
+
+            print("[INFO ] Deleting superseded package: " +
+                  entry["display_name"])
+
+            entry_obj = launchpad.load(entry['self_link'])
+            entry_obj.requestDeletion(
+                removal_comment='Automated removal of superseded package.')
+
+    print("[INFO ] Script complete!")
