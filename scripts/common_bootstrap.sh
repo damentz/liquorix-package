e003cbe2412f98ee1cb3694700ec7d4c577badf9
Steven Barrett <steven@liquorix.net>
2018-04-18 14:19:32 -0500

    common_bootstrap.sh: Fix source line

--- scripts/common_bootstrap.sh
+++ scripts/common_bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-￼source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
 
 if [[ ! -f "$dir_base/$package_source" ]]; then
     echo "[WARN ] Missing source file: $dir_base/$package_source, downloading now."
