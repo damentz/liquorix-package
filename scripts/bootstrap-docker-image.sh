77b385d8c98926f90397fcac660057a04ca45c33
Steven Barrett <steven@liquorix.net>
2018-04-18 10:53:54 -0500

    docker: Add some basic input handling to image script

--- scripts/bootstrap-docker-image.sh
+++ scripts/bootstrap-docker-image.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-declare distro="$1"
-declare release="$2"
+set -euo pipefail
+
+declare distro=${1:-}
+declare release=${2:-}
+
+declare -i fail=0
+
+if [[ -z "$distro" ]]; then
+    echo "[ERROR] No distribution set!"
+    fail=1
+fi
+
+if [[ -z "$release" ]]; then
+    echo "[ERROR] No release set!"
+    fail=1
+fi
+
+if [[ $fail -eq 1 ]]; then
+    echo "[ERROR] Encountered a fatal error, cannot continue!"
+    exit 1
+fi
 
 declare release_string="liquorix_$distro/$release"
 if docker image ls | grep "$release_string" -q; then
