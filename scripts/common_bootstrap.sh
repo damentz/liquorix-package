fab0def4742ac96d0236b1498e7f9ad08f2feb43
Steven Barrett <steven@liquorix.net>
2018-04-29 15:50:53 -0500

    common_bootstrap.sh: Archive gnupg for docker usage

--- scripts/common_bootstrap.sh
+++ scripts/common_bootstrap.sh
@@ -8,3 +8,13 @@ if [[ ! -f "$dir_base/$package_source" ]]; then
     echo "[WARN ] Missing source file: $dir_base/$package_source, downloading now."
     wget -O "$dir_base/$package_source" "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-${version_kernel}.tar.xz"
 fi
+
+declare gnupg_archive="$dir_base/gnupg.tar.gz"
+if [[ ! -f "$gnupg_archive" ]]; then
+    echo "[WARN ] Missing gnupg archive file: $dir_base/$gnupg_archive, creating now."
+    
+    cd ~/
+    tar -czvpf "$gnupg_archive" .gnupg/ || {
+        echo "[ERROR] No gnupg configuration found, docker images will not build and package signing will fail!"
+    }
+fi
