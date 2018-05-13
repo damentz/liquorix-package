767234e52b193a018aa97bdaced4bc269bc4131c
Steven Barrett <steven@liquorix.net>
2018-05-13 17:31:35 -0500

    docker: Add debian package build script

---
+++ scripts/docker_build-binary_debian.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
+
+declare -i processes_default=2
+declare -i processes=${1:-"$processes_default"}
+declare -i build=${2:-${version_build}}
+declare -a args=()
+
+declare distro=''
+
+for arch in 'amd64' 'i386'; do
+    distro='debian'
+    for release in "${releases_debian[@]}"; do
+        args+=("$arch" "$distro" "$release" "$build")
+    done
+done
+
+echo "[DEBUG] $0, args: ${args[@]}"
+for item in "${args[@]}"; do
+    echo "$item"
+done | xargs -n3 -P "$processes" "$dir_base/scripts/docker_build-binary.sh"
