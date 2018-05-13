56670787a56a2986f13ddcf5278ea1c25e2be361
Steven Barrett <steven@liquorix.net>
2018-05-13 17:54:53 -0500

    Add liquorix release script

---
+++ scripts/make-liquorix-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
+
+
+declare build=${1:-${version_build}}
+declare dir_scripts="$dir_base/scripts"
+
+declare -i procs=0
+if which nproc > /dev/null 2>&1; then
+    procs=$(( $(nproc) / 2 ))
+fi
+
+if [[ $procs < 2 ]]; then
+    procs=2
+fi
+
+schedtool -D -n19 -e $dir_scripts/docker_bootstrap.sh $procs
+schedtool -D -n19 -e $dir_scripts/docker_build-source_all.sh $procs $build
+schedtool -D -n19 -e $dir_scripts/docker_submit-ppa-sources.sh $build
+schedtool -D -n19 -e $dir_scripts/docker_build-binary_debian.sh $build
