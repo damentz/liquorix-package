104acd00b60b7da43604bf1543892892f06c41a4
Steven Barrett <steven@liquorix.net>
2018-05-13 16:02:46 -0500

    docker: Factor out source building to separate script

---
+++ scripts/docker_build-sources-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
+
+declare -i processes_default=2
+declare -i processes=${1:-"$processes_default"}
+declare -i build=${2:-${version_build}}
+declare -a args=()
+
+declare distro=''
+
+distro='ubuntu'
+for release in "${releases_ubuntu[@]}"; do
+    args+=("$distro" "$release" "$build")
+done
+
+distro='debian'
+for release in "${releases_debian[@]}"; do
+    args+=("$distro" "$release" "$build")
+done
+
+echo "[DEBUG] $0, args: ${args[@]}"
+for item in "${args[@]}"; do
+    echo "$item"
+done | xargs -n3 -P "$processes" "$dir_base/scripts/docker_build-sources.sh"
--- scripts/docker_submit-ppa-sources.sh
+++ scripts/docker_submit-ppa-sources.sh
@@ -6,21 +6,11 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
 
 declare -i processes_default=2
 declare -i processes=${1:-"$processes_default"}
-declare -a releases=("${releases_ubuntu[@]}")
 declare -i build=${2:-${version_build}}
 declare -a args=()
 
 declare distro='ubuntu'
-for release in "${releases[@]}"; do
-    args+=("$distro" "$release" "$build")
-done
-
-echo "[DEBUG] $0, args: ${args[@]}"
-for item in "${args[@]}"; do
-    echo "$item"
-done | xargs -n3 -P "$processes" "$dir_base/scripts/docker_build-sources.sh"
-
-for release in "${releases[@]}"; do
+for release in "${releases_ubuntu[@]}"; do
     echo "[INFO ] Uploading sources for $distro/$release"
     docker run \
     --rm \
