df2a4331cb9a903dae62a334e41d485e4f9069e5
Steven Barrett <steven@liquorix.net>
2018-05-13 17:49:57 -0500

    docker_submit-ppa-sources.sh: Remove process count argument, unused

--- scripts/docker_submit-ppa-sources.sh
+++ scripts/docker_submit-ppa-sources.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
 
-declare -i processes_default=2
-declare -i processes=${1:-"$processes_default"}
-declare -i build=${2:-${version_build}}
+declare -i build=${1:-${version_build}}
 declare -a args=()
 
 declare distro='ubuntu'
