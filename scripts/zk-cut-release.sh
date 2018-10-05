774f00851eab7d6a0936f59f34e95c591188799a
Steven Barrett <steven@liquorix.net>
2018-10-05 13:34:40 -0500

    Add 'zk-cut-release.sh' to standardize zen-kernel tag development

---
+++ scripts/zk-cut-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare ev=${1:-lqx1}
+if [[ ! -f "Makefile" ]]; then
+    echo "[ERROR] Makefile for Linux not in current directory!"
+    exit 1
+fi
+
+sed -r -i "s/^EXTRAVERSION = .*/EXTRAVERSION = -${ev}/" Makefile
+
+declare -i kv=$(grep '^VERSION = ' Makefile | sed -r 's/^VERSION = //')
+declare -i kpl=$(grep '^PATCHLEVEL = ' Makefile | sed -r 's/^PATCHLEVEL = //')
+declare -i ksl=$(grep '^SUBLEVEL = ' Makefile | sed -r 's/^SUBLEVEL = //')
+
+tag="v$kv.$kpl.$ksl-$ev"
+
+git add Makefile
+git commit -m "Cut $tag"
+git tag -s $tag -m "Cut $tag"
+git push origin $tag
