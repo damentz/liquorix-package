22362fdba20333db0357a1dc48add726d3a413bb
Steven Barrett <steven@liquorix.net>
2018-10-07 15:09:33 -0500

    docker_build-binary_debian.sh: Standardize random different pattern

--- scripts/docker_build-binary_debian.sh
+++ scripts/docker_build-binary_debian.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
 
 declare -i processes_default=2
-declare -i processes=${1:-"$processes_default"}
+declare -i processes=${1:-${processes_default}}
 declare -i build=${2:-${version_build}}
 declare -a args=()
 
