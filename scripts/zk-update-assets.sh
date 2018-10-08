43772ac64c9218fa40aadb628fe77725a3091784
Steven Barrett <steven@liquorix.net>
2018-10-07 20:41:52 -0500

    zk-update-assets.sh: Clean up 'release_data' assignment

--- scripts/zk-update-assets.sh
+++ scripts/zk-update-assets.sh
@@ -18,13 +18,14 @@ declare -i kpl=$(grep -E '^PATCHLEVEL = ' Makefile | sed -r 's/^PATCHLEVEL = //'
 declare -i ksl=$(grep -E '^SUBLEVEL = ' Makefile | sed -r 's/^SUBLEVEL = //')
 
 tag="v$kv.$kpl.$ksl-$ev"
+tag_commit="$(git rev-list -n1 "$tag")"
 tag_patch_file="${tag}.patch.xz"
 tag_patch_dir="../"
 
 # zen-kernel repo is under an organization, not user, so it must be specified
 # by its unique ID
 declare -i repository_id=2465166
-release_data="{\"tag_name\": \"$tag\", \"tag_commitish\": \"$(git rev-list -n1 $tag)\"}"
+release_data='{"tag_name": "'"$tag"'", "tag_commitish": "'"$tag_commit"'"}'
 
 git diff "v${kv}.$kpl" "$tag" | xz -9 > "$tag_patch_dir/$tag_patch_file"
 gpg --output "$tag_patch_dir/$tag_patch_file.sig" \
