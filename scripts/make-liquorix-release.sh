f5510fcfe0f8ca11af6452cb589df900de1b33bc
Steven Barrett <steven@liquorix.net>
2019-05-22 19:24:30 -0500

    make-liquorix-release.sh: Quote input params

--- scripts/make-liquorix-release.sh
+++ scripts/make-liquorix-release.sh
@@ -18,11 +18,11 @@ if [[ $procs -lt 2 ]]; then
     procs=2
 fi
 
-"$dir_scripts"/docker_bootstrap.sh $procs
-"$dir_scripts"/docker_build-source_all.sh $procs $build
-"$dir_scripts"/docker_submit-ppa-sources.sh $build
+"$dir_scripts"/docker_bootstrap.sh "$procs"
+"$dir_scripts"/docker_build-source_all.sh "$procs" "$build"
+"$dir_scripts"/docker_submit-ppa-sources.sh "$build"
 
 # We build only one kernel at a time since each build uses all CPU resources
-"$dir_scripts"/docker_build-binary_debian.sh 1 $build
+"$dir_scripts"/docker_build-binary_debian.sh 1 "$build"
 
-"$dir_scripts"/repo_add-debian-packages.sh $build
+"$dir_scripts"/repo_add-debian-packages.sh "$build"
