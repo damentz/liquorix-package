842cef8d0e8b9edfd834cba00f33e77cb010c016
Steven Barrett <steven@liquorix.net>
2020-08-23 11:45:07 -0500

    common_bootstrap.sh: Quiet wget output

--- scripts/debian/common_bootstrap.sh
+++ scripts/debian/common_bootstrap.sh
@@ -7,5 +7,5 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
 
 if [[ ! -f "$dir_base/$package_source" ]]; then
     echo "[WARN ] Missing source file: $dir_base/$package_source, downloading now."
-    wget -O "$dir_base/$package_source" "https://cdn.kernel.org/pub/linux/kernel/v${version_major}.x/linux-${version_kernel}.tar.xz"
+    wget --quiet -O "$dir_base/$package_source" "https://cdn.kernel.org/pub/linux/kernel/v${version_major}.x/linux-${version_kernel}.tar.xz"
 fi
