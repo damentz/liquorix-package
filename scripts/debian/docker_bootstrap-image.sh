ba9aeac17149288ddbe72de1ad7c5771fdcfdaaa
Steven Barrett <steven@liquorix.net>
2025-02-05 17:05:07 -0600

    Revert "Pull images from ECR public gallery first"
    This reverts commit 4eab2ce56e690980c4f254b86654399e750fe7c8.

--- scripts/debian/docker_bootstrap-image.sh
+++ scripts/debian/docker_bootstrap-image.sh
@@ -81,8 +81,6 @@ if [[ "$(docker image ls)" == *"$release_string"* ]]; then
     docker container rm "$container_id" > /dev/null
 else
     echo "[INFO ] $release_string: Docker image not found, building with Dockerfile."
-    docker pull "public.ecr.aws/$distro/$distro:$release"
-    docker tag "public.ecr.aws/$distro/$distro:$release" "$arch/$distro:$release"
     docker buildx build \
         --network="host" \
         --no-cache \
